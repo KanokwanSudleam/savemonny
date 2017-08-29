0dad03501c7e2e227a7f0dfe6ad3635afe4de301
KBANKNET\kanokwan.sud <kanokwan.sud@kbtg.tech>
2017-08-29 15:59:53 +0700

    edit form

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/',function(req,res){
+	var description = req.body.description
+	var channel=req.body.channel
+	console.log('description',description)
+	console.log(channel)
+	 res.render('index', { title: 'Express' })
+})
 
 module.exports = router;
